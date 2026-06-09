10113cd27e261b27ab05325c99f97703c2d7edcf
Sanjiv-Kumar-AIML-RMDEC <25al094@rmd.ac.in>
2026-06-09 11:01:52 +0530

    Update Adjacent-Cell-Sum.java

--- Adjacent-Cell-Sum.java
+++ Adjacent-Cell-Sum.java
@@ -1,13 +1,13 @@
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int R = sc.nextInt(), C = sc.nextInt();
+        int R = sc.nextInt();
+        int C = sc.nextInt();
 
-        int grid[][] = new int[R][C];
+        int[][] grid = new int[R][C];
 
         for (int row = 0; row < R; row++) {
             for (int col = 0; col < C; col++) {
@@ -15,24 +15,23 @@ public class Main {
             }
         }
 
-        int row = sc.nextInt(), col = sc.nextInt();
+        int targetRow = sc.nextInt();
+        int targetCol = sc.nextInt();
 
-        int diff[][] = {
+        int[][] diff = {
             {-1, -1}, {-1, 0}, {-1, 1},
-            {0, -1}, {0, 1},
+            {0, -1},           {0, 1},
             {1, -1},  {1, 0},  {1, 1}
         };
 
         long sum = 0;
 
         for (int i = 0; i < 8; i++) {
-
-            int adjRow = row + diff[i][0];
-            int adjCol = col + diff[i][1];
+            int adjRow = targetRow + diff[i][0];
+            int adjCol = targetCol + diff[i][1];
 
             if (adjRow >= 0 && adjRow < R &&
                 adjCol >= 0 && adjCol < C) {
-
                 sum += grid[adjRow][adjCol];
             }
         }
